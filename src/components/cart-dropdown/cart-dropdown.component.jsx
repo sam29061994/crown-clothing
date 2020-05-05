bbb5328aaa12280395e97f15f6a0ecdb2c09fe37
sam <saumilbarot5@gmail.com>
2020-05-05 18:00:21 -0400

    updating application with reselect and fixing the vulnerabilities on package.jason

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { selectCartItems } from '../../redux/cart/cart.selector';
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import '../cart-dropdown/cart-dropdown.styles.scss';
@@ -8,12 +9,12 @@ const CartDropDown = ({ cartItems }) => (
     <div className="cart-dropdown">
         <div className="cart-items">
             {cartItems.map((cartItem) => (
-                <CartItem key={cartItem.id} item = {cartItem} />
+                <CartItem key={cartItem.id} item={cartItem} />
             ))}
         </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 );
-const mapStateToProps = ({ cart: { cartItems } }) => ({ cartItems });
+const mapStateToProps = (state) => ({ cartItems: selectCartItems(state) });
 
 export default connect(mapStateToProps)(CartDropDown);
