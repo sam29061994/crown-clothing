7edd195316125d171b98d502a8a5be38de93c779
sam <saumilbarot5@gmail.com>
2020-05-06 16:03:48 -0400

    added reducers and removed states from shop page added new shop overveiw component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,11 @@
-import React from "react";
-import SHOP_DATA from "./shop.data";
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import React from 'react';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
 
-class Shop extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-  render() {
-    return (
-      <div className="shop-page">
-        {this.state.collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview
-            key={id}
-            {...otherCollectionProps}
-          ></CollectionPreview>
-        ))}
-      </div>
-    );
-  }
-}
+const Shop = () => (
+    <div className="shop-page">
+       <CollectionsOverview/>
+    </div>
+);
 
 export default Shop;
